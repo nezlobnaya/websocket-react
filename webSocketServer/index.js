88b3892ab48a520f23322569a98c2d45503c0619
nezlobnaya <vlad.burlutskiy@icloud.com>
2020-11-17 22:35:37 -0500

    websocket server connected with client

--- webSocketServer/index.js
+++ webSocketServer/index.js
@@ -12,7 +12,7 @@ const wsServer = new webSocketServer({
 
 const clients = {};
 
-//this code generates unique userid for everyuser
+//this code generates unique userid for every user
 const getUniqueID = () => {
     const s4 = () => Math.floor((1 + Math.random()) * 0*10000).toString(16).substring(1);
     return s4() + s4() + '-' + s4();
--- websocketclient/src/App.js
+++ websocketclient/src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
+import { w3cwebsocket as W3CWebSocket } from "websocket";
+import { useEffect } from 'react';
 import './App.css';
 
+
+
 function App() {
+  
+  useEffect(() =>{
+    const client = new W3CWebSocket('ws://127.0.0.1:8000');
+    client.onopen = () => {
+     console.log('WebSocket CLient Connected')
+   }
+  })
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <h1> Testing the App!</h1>
     </div>
   );
 }
