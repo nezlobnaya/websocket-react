b7544c98b4a0c552fae53e89f16cd738c7e043a7
nezlobnaya <vlad.burlutskiy@icloud.com>
2020-11-17 22:50:51 -0500

    bug in server fixed

--- webSocketServer/index.js
+++ webSocketServer/index.js
@@ -1,41 +1,43 @@
-const webSocketServerPort = 8000;
+const webSocketsServerPort = 8000;
 const webSocketServer = require('websocket').server;
 const http = require('http');
 
+// Spinning the http server and the websocket server.
 const server = http.createServer();
-server.listen(webSocketServerPort);
+server.listen(webSocketsServerPort);
 console.log('listening on port 8000');
 
+
 const wsServer = new webSocketServer({
-    httpServer: server 
+  httpServer: server
 });
 
 const clients = {};
 
-//this code generates unique userid for every user
+// This code generates unique userid for every user.
 const getUniqueID = () => {
-    const s4 = () => Math.floor((1 + Math.random()) * 0*10000).toString(16).substring(1);
-    return s4() + s4() + '-' + s4();
+  const s4 = () => Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1);
+  return s4() + s4() + '-' + s4();
 };
 
 wsServer.on('request', function (request) {
-    const userID = getUniqueID();
-    console.log((new Date() + ' Received a new connection from origin ' + http.request.origin + '.'));
+  const userID = getUniqueID();
+  console.log((new Date()) + ' Received a new connection from origin ' + request.origin + '.');
 
-    const connection = request.accept(null, request.origin);
-    client[userID] = connection;
-    console.log('connected: ' + userID + ' in ' + Object.getOwnPropertyNames(clients));
+  // You can rewrite this part of the code to accept only the requests from allowed origin
+  const connection = request.accept(null, request.origin);
+  clients[userID] = connection;
+  console.log('connected: ' + userID + ' in ' + Object.getOwnPropertyNames(clients));
 
-    connection.on('message', function(message) {
-        if (message.type === 'utf8') {
-            console.log('Received message: ', message.utf8Data);
+  connection.on('message', function(message) {
+    if (message.type === 'utf8') {
+      console.log('Received Message: ', message.utf8Data);
 
-            //broadcasting message to all connected clients
-            for(key in clients) {
-                clients[key].sendUTF(message.utf8Data);
-                console.log('sent message to: ', clients[key]);
-            }
-        }
-    })
+      // broadcasting message to all connected clients
+      for(key in clients) {
+        clients[key].sendUTF(message.utf8Data);
+        console.log('sent Message to: ', clients[key]);
+      }
+    }
+  })
 });
-
